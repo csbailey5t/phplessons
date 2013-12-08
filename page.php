ead46b1c8f54044224dd900132f8c1f352ae8649
Scott Bailey <csb5t@virginia.edu>
2013-12-08 16:47:39 -0500

    used if statement to see if the variable passed in url is equivalent to an array key, and if so it echos what the person is bringing. I know this isn't the most elegant solution, but I'm still working on figuring out how to pull out an array key as a variable I can check against.

--- page.php
+++ page.php
@@ -11,9 +11,52 @@
 		
 		$name = $_GET['name'];
 
+		$contrib = array(
+			"Scott" => "spicy cream corn.",
+			"Jeremy" => "smoked pork shoulder or chicken soup.",
+			"Bethany" => "wine and cheese.",
+			"Eric" => "mac and cheese.",
+			"Veronica" => "stuffed squash and bbq pulled chicken.",
+			"Steph" => "spinach pie.",
+			"Francesca" => "big, tasty salad.",
+			"Ronda" => "baked dessert.");
+
+		
+		if ($name == "Scott") {
+		echo $name." is bringing ".$contrib["$name"];
+		};
+
+
+		if ($name == "Jeremy") {
+		echo $name." is bringing ".$contrib["$name"];
+		};
+
+		if ($name == "Bethany") {
+		echo $name." is bringing ".$contrib["$name"];
+		};
+
+		if ($name == "Eric") {
+		echo $name." is bringing ".$contrib["$name"];
+		};
+
+		if ($name == "Veronica") {
+		echo $name." is bringing ".$contrib["$name"];
+		};
+
+		if ($name == "Steph") {
+		echo $name." is bringing ".$contrib["$name"];
+		};
+
+		if ($name == "Francesca") {
+		echo $name." is bringing ".$contrib["$name"];
+		};
+
+		if ($name == "Ronda") {
+		echo $name." is bringing ".$contrib["$name"];
+		};
 		
 	
-		echo $name;
+		
 	?>
 
 
