f62fbf33db92d7b5f36e0e9e1f92daf90703de5e
Scott Bailey <csb5t@virginia.edu>
2013-12-09 18:40:46 -0500

    first commit. use if and elseif. don't know why it doesn't work.

---
+++ time.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Time Traveler Problem</title>
+</head>
+<body>
+
+	<?php
+
+		$year = 2035;
+
+		if ($year < 1900) {
+			echo "Welcome wonderful visitor from the past!";
+		} elseif ($year <= 1990 && $year <= 2020) {
+			echo "Welcome visitor from the relatively contemporary time period. Couldn't find somewhen better?";
+		} else {
+			echo "Welcome visitor from the far future. How did we last that long?";
+		}
+	?>
+	
+</body>
+</html>
