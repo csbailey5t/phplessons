96370eece9993a3312e84f1809a6eda5943d72d6
Scott Bailey <csb5t@virginia.edu>
2013-12-13 15:24:41 -0500

    initial commit, with working content

---
+++ reverse.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Reverse String</title>
+</head>
+<body>
+	
+	<?php
+    
+    function reverse_string($string) {
+    	return strrev($string);
+    } 
+
+    $string = "Here is my String!";
+
+    $reversed_string = reverse_string($string);
+
+    echo "If you reverse '$string,' you get '$reversed_string.'";
+
+
+	?>
+
+</body>
+</html>
