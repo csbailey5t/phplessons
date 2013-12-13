e887ecc0cff4b9732a41771243896a9a915fb8f2
Scott Bailey <csb5t@virginia.edu>
2013-12-13 12:00:36 -0500

    initial commit, with full solution

---
+++ multiply.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Multiplication</title>
+</head>
+<body>
+
+	<?php
+	function multiply($x, $y)
+		{
+			return $x * $y;
+		}
+
+	$integers = array(
+		4 => 2,
+		0 => 4,
+		900 => 32,
+		299999 => 23);
+
+	foreach ($integers as $x => $y) {
+		$result = multiply($x, $y);
+		echo "When you multiple $x and $y, you get $result <br>";
+	}
+
+	?>
+
+	
+</body>
+</html>
