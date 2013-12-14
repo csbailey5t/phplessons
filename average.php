d35cf3908c3bdae04da2aaeddb9c0a2e5a08af19
Scott Bailey <csb5t@virginia.edu>
2013-12-14 10:28:03 -0500

    added a second way to compute the average using a foreach loop.

--- average.php
+++ average.php
@@ -5,9 +5,10 @@
 	<title>Average</title>
 </head>
 <body>
-	<?php
 
-	/* Wayne suggested using a foreach loop with running sum, but here is a way without that */
+	<p> Wayne suggested using a foreach loop with running sum, but here is a way without that.</p>
+
+	<?php
 
     function average($numbers){
     	return array_sum($numbers) / count($numbers);
@@ -18,6 +19,25 @@
     $average = average($numbers);
 
     echo "$average is the average of the array;"
+
+    ?>
+
+    <p>Here is the second version of this, done with the foreach loop.</p>
+
+    <?php 
+
+    function average_loop($numbers) {
+    	$sum = 0;
+    	foreach ($numbers as $number) {
+    		$sum += $number;
+    	};
+    	return $sum / count($numbers);
+    	}
+    
+    $numbers = array(1, 2, 3, 4, 5);
+
+    $average_two = average_loop($numbers);
+    echo "$average_two";
 
     ?>
 </body>
